d3b03a6e66bfcaf46231a67613545bebc94f8c2b
jakkapet2k <jakkapet.l@gmail.com>
2023-01-10 14:31:18 +0700

    app_dev week7

---
+++ src/week7/ArrayListExample1.java
@@ -0,0 +1,20 @@
+package week7;
+
+// https://www.javatpoint.com/java-arraylist
+
+import java.util.*;
+
+public class ArrayListExample1 {
+	public static void main(String args[]) {
+		ArrayList<String> list = new ArrayList<String>();// Creating arraylist
+		list.add("Mango");// Adding object in arraylist
+		list.add("Apple");
+		list.add("Banana");
+		list.add("Grapes");
+		list.add("Papaya");
+		list.add("Tun");
+		list.add("Oren");
+		// Printing the arraylist object
+		System.out.println(list);
+	}
+}
